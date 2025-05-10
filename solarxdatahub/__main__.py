6b5903eaed98583a946b690915d0918009bc2b31
C0deByCata <rotarucatalinr@gmail.com>
2025-05-10 19:48:30 +0200

    refactor: Remove environment preparation and streamline main execution flow

--- solarxdatahub/__main__.py
+++ solarxdatahub/__main__.py
@@ -2,25 +2,11 @@
 
 import logging
 
-from solarxdatahub.config import Logging
+from solarxdatahub.core.controller import run
 
 logger = logging.getLogger(__name__)
 
 
-def prepare_environment():
-    """Prepare the environment for the process.
-
-    Raises:
-        ControlledException: If the ID of a product is not found.
-    """
-    logging.basicConfig(
-        level=Logging.LEVEL,
-        format=Logging.FORMAT,
-        datefmt=Logging.DATEFMT,
-    )
-    logger.info("Environment prepared")
-
-
 if __name__ == "__main__":
-    prepare_environment()
-    logger.info("Application started")
+    run()
+    logger.info("Data hub completed")
